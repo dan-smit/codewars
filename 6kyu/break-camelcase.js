79c48da807cdde986b7378db2855bb03e55888e7
Daniel Smith <danielsmithche@gmail.com>
2023-06-01 08:15:01 -0500

    daily codewars

---
+++ 6kyu/break-camelcase.js
@@ -0,0 +1,12 @@
+// Complete the solution so that the function will break up camel casing, using a space between words.
+
+// Example
+// "camelCasing"  =>  "camel Casing"
+// "identifier"   =>  "identifier"
+// ""             =>  ""
+
+//My Solution
+
+function solution(string) {
+    return string.split('').map(letter => letter == letter.toUpperCase() ? ` ${letter}` : letter).join('')
+}
