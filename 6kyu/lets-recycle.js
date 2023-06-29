916c9e15894d879233d3df026cd99be22db1bccc
Daniel Smith <danielsmithche@gmail.com>
2023-06-28 19:15:51 -0500

    daily codewars

---
+++ 6kyu/lets-recycle.js
@@ -0,0 +1,15 @@
+
+
+//My solution
+
+function recycle(array){
+    let paper = [], glass = [], organic = [], plastic = []
+    array.forEach(obj => {
+        if(obj.material === 'paper' || obj.secondMaterial === 'paper') paper.push(obj.type)
+        if(obj.material === 'glass' || obj.secondMaterial === 'glass') glass.push(obj.type)
+        if(obj.material === 'organic' || obj.secondMaterial === 'organic') organic.push(obj.type)
+        if(obj.material === 'plastic' || obj.secondMaterial === 'plastic') plastic.push(obj.type)
+
+    })
+    return [paper, glass, organic, plastic]
+}
