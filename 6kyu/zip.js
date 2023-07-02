0d33ecd91f4ede97a9f27bd5970592cd964f9e5b
Daniel Smith <danielsmithche@gmail.com>
2023-07-02 15:55:56 -0500

    daily codewars

--- 6kyu/zip.js
+++ 6kyu/zip.js
@@ -18,3 +18,18 @@
 
 //My Solution
 
+function zipObject(keys, values){
+    //Initialize an empty object to store key-value pairs
+    let result = {};
+    //iterate over keys array and check to see if it is an array
+    (keys || []).forEach(function(key, index){
+        //check to see if key is an array. If it is, treat it as a pair where the first element is key, second is value
+        //If not an array, assume the key should directly correspond to an element in the 'values' array
+        if(key instanceof Array){
+            result[key[0]] = key[1]
+        }else{
+            result[key] = (values || [])[index]
+        }
+    })
+    return result
+}
