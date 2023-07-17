4a273d211fcb24c8a3ada94f09606475a46d300d
Daniel Smith <danielsmithche@gmail.com>
2023-07-17 09:41:20 -0500

    made a change

--- 7kyu/who-took-the-car-keys.js
+++ 7kyu/who-took-the-car-keys.js
@@ -7,7 +7,7 @@
 // > whoTookTheCarKey(['01000001', '01101100', '01100101', '01111000', '01100001', '01101110', '01100100', '01100101', '01110010'])
 // 'Alexander'
 
-//My Solution
+//My Solution edited
 
 function whoTookTheCarKey(message) {
     return message.map((l) => String.fromCharCode(parseInt(l, 2))).join('');
