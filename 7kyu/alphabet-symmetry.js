238a297192c2d983ef6bd9159dbfbbe865d23ef5
Daniel Smith <danielsmithche@gmail.com>
2023-07-21 11:42:19 -0500

    daily codewars

---
+++ 7kyu/alphabet-symmetry.js
@@ -0,0 +1,28 @@
+// Consider the word "abode". We can see that the letter a is in position 1 and b is in position 2. In the alphabet, a and b are also in positions 1 and 2. Notice also that d and e in abode occupy the positions they would occupy in the alphabet, which are positions 4 and 5.
+
+// Given an array of words, return an array of the number of letters that occupy their positions in the alphabet for each word. For example,
+
+// solve(["abode","ABc","xyzD"]) = [4, 3, 1]
+// See test cases for more examples.
+
+// Input will consist of alphabet characters, both uppercase and lowercase. No spaces.
+
+// Good luck!
+
+//My Solution
+
+function solve(arr){  
+    let arrLower = arr.map(word => word.toLowerCase())
+    return arrLower.map(word => {
+      return symmetry(word)
+    })
+  };
+  
+  function symmetry(str){
+    let alph = 'abcdefghijklmnopqrstuvwxyz'
+    let arr = str.split('')
+    let count = 0
+    return arr.filter(function(letter, ind, arr){
+      return alph.indexOf(letter) == ind
+    }).length
+  }
