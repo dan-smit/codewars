dbc4cef5215ff6cfbdf884bb0e9ee8862823612b
Daniel Smith <danielsmithche@gmail.com>
2023-07-22 21:07:30 -0500

    daily codewars

---
+++ 7kyu/two-nums-are-pos.js
@@ -0,0 +1,21 @@
+// Your job is to write a function, which takes three integers a, b, and c as arguments, and returns True if exactly two of of the three integers are positive numbers (greater than zero), and False - otherwise.
+
+// Examples:
+// twoArePositive(2, 4, -3) == true
+// twoArePositive(-4, 6, 8) == true
+// twoArePositive(4, -6, 9) == true
+// twoArePositive(-4, 6, 0) == false
+// twoArePositive(4, 6, 10) == false
+// twoArePositive(-14, -3, -4) == false
+
+//My Solution
+
+function twoArePositive(a, b, c) {
+    function cond(val){
+      return val>0
+    }
+    if(cond(a) && cond(b) && !cond(c)) return true
+    if(cond(a) && cond(c) && !cond(b)) return true
+    if(cond(b) && cond(c) && !cond(a)) return true
+    return false
+  }
