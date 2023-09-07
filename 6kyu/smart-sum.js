1be7af058cb337faa97d97acac8dce730b7b7bf3
Daniel Smith <danielsmithche@gmail.com>
2023-09-07 17:59:26 -0500

    daily codewars

--- 6kyu/smart-sum.js
+++ 6kyu/smart-sum.js
@@ -4,3 +4,17 @@
 // smartSum(1,2,[[3,4],5],6); // returns 21
 
 // My Solution
+
+function smartSum(){
+    let sum = 0;
+    for (let i = 0; i < arguments.length; i++) {
+      if (typeof arguments[i] === 'number') {
+        sum += arguments[i];
+      } else {
+        for (let j = 0; j < arguments[i].length; j++) {
+          sum += smartSum(arguments[i][j]);
+        }
+      }
+    }
+    return sum;
+  }
