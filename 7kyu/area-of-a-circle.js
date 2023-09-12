640a194ce2a8583b318de4d37b9e8ec2eacb9eb0
Daniel Smith <danielsmithche@gmail.com>
2023-09-11 19:51:37 -0500

    daily codewars

---
+++ 7kyu/area-of-a-circle.js
@@ -0,0 +1,21 @@
+// Complete the function which will return the area of a circle with the given radius.
+
+// Returned value is expected to be accurate up to tolerance of 0.01.
+
+// If the radius is not positive, throw Error.
+
+// Example:
+
+// circleArea(43.2673);     // returns 5881.248  (± 0.01)
+// circleArea(68);          // returns 14526.724 (± 0.01)
+// circleArea(0);           // throws Error
+// circleArea(-1);          // throws Error
+
+//My Solution
+
+function circleArea(radius) {
+    if(radius <= 0){
+      throw new Error('Radius must be positive.')
+    }
+   return Math.PI * radius * radius
+ }
